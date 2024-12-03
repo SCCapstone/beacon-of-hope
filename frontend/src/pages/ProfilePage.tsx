79fdf272e996f090821bdcfca1ca78fb534b3fa2
andyjDavison <andyd.soccer@gmail.com>
2024-12-03 13:23:02 -0500

    Added CSS Styling to Login page

--- frontend/src/pages/ProfilePage.tsx
+++ frontend/src/pages/ProfilePage.tsx
@@ -3,7 +3,7 @@ import UserProfileCard from "../components/FoodPreferencesCards/UserProfileCard"
 import FoodPreferencesCard from "../components/FoodPreferencesCards/FoodPreferencesCard";
 import MealPlanCard from "../components/FoodPreferencesCards/MealPlanCard";
 import MealPlanStats from "../components/FoodPreferencesCards/MealPlanStats";
-import "./ProfilePage.css";
+//import "./ProfilePage.css";
 
 const ProfilePage: React.FC = () => {
   return (
