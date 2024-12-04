cc00fe8cf03de215d279355100abeb9bdbd038b6
Vansh Nagpal <2002vansh@gmail.com>
2024-12-04 17:08:34 -0500

    changing allowed IP addresses for CORS

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -129,7 +129,7 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 CORS_ALLOW_ALL_ORIGINS = True
 
 CORS_ALLOWED_ORIGINS = [
-    "http://localhost:3000",  # React or other frontend on localhost
+    "http://localhost:5173",  # React or other frontend on localhost
     # "https://your-production-domain.com",
 ]
 
